ee7c34ef692045023ef77e08c1d6e9b2cc9d42ff
chekonomist <don.sergio.guerrero@gmail.com>
2018-08-17 16:37:55 -0500

    third hw done

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+const ejs = require('ejs')
 const express = require('express')
+
 const pageRouter = require('./src/routes/pageRouter.js')
 const apiRouter = require('./src/routes/apiRouter.js')
 
 const app = express()
 const PORT = 3000
 
+app.use( express.static( `${__dirname}/public` ) )
+
+app.engine( 'ejs', ejs.renderFile )
+app.set('view engine', 'ejs')
+app.set('views', `${__dirname}/src/views`)
+
 app.use('/', pageRouter)
 app.use('/api', apiRouter)
 app.use((req, res)=>{
-  res.send('<h3> 404 - big error - page no exist</h3>')
+  res.render('404.ejs')
 })
 
 app.listen(PORT, ()=>{
